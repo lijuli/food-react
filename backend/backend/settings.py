f449dd83db8c8bcaf2b64b421d0c9b34f5475655
Julie <julia.mysyuk@gmail.com>
2021-10-21 20:12:33 -0400

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,7 +119,6 @@ DJOSER = {
     'PERMISSIONS': {
         'user_list': ['rest_framework.permissions.AllowAny'],
         'user': ['rest_framework.permissions.IsAuthenticated'],
-        'set_password': ['rest_framework.permissions.IsAdminUser']
     },
     'HIDE_USERS': False,
 }
